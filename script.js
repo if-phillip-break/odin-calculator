31fe14f565c6f461207c68879d8bf27bb99b78a2
if-phillip-break <Philtran2003@icloud.com>
2023-08-27 07:00:34 -0400

    Made progress in performing operation calculation

--- script.js
+++ script.js
@@ -7,27 +7,22 @@ let valueArr = [];
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        displayValue.textContent = button.textContent;
-        // switch (button.textContent){
-        //     case "+":
-        //         operator = button.textContent;
-        //         break;
-        //     case "-":
-        //         operator = button.textContent;
-        //         break;
-        //     case "*":
-        //         operator = button.textContent;
-        //         break;
-        //     case "/":
-        //         operator = button.textContent;
-        //         break;
-        //     case "=":
-        //         operator = button.textContent;
-        //          break;
-        // }
-        valueArr.push(button.textContent)
-    });
+    if (button.textContent === "="){
+        button.addEventListener('click', () => {
+            valueArr.push(button.textContent)
+            calculate(groupNum(valueArr));
+        });
+    } else if (button.textContent === "Clear"){
+        button.addEventListener('click', () => {
+            displayValue.textContent = ""
+            valueArr = [];
+        });
+    } else {
+        button.addEventListener('click', () => {
+            displayValue.textContent += button.textContent;
+            valueArr.push(button.textContent)
+        });
+    }
 });
 
 function add(num1, num2){
@@ -44,6 +39,27 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     return num1 / num2;
+}
+
+function groupNum(arr){
+    let numArray = []
+    let num = "";
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === "+" || arr[i] === "-" || arr[i] === "*" || arr[i] === "/" || arr[i] === "="){
+            num = Number(num);
+            numArray.push(num);
+            numArray.push(arr[i]);
+            num = "";
+        } else {
+            num += arr[i];
+        }
+    }
+    return numArray;
+}
+
+function calculate(numArray){
+    
 }
 
 function operate(operator, num1, num2){
