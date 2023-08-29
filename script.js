a2fa16b7bf72db0dc93b6ecf7c1fa3c953836210
if-phillip-break <Philtran2003@icloud.com>
2023-08-29 08:17:48 -0400

    Fixed calculate function (program working for most part)

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-let num1;
-let num2;
-let operator;
+let num1 = "";
+let num2 = "";
+let operator = "";
+let num = "";
 
 let displayValue = document.querySelector('.display');
-let valueArr = [];
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    if (button.textContent === "="){
-        button.addEventListener('click', () => {
-            valueArr.push(button.textContent)
-            calculate(groupNum(valueArr));
-        });
-    } else if (button.textContent === "Clear"){
+    if (button.textContent === "Clear"){
         button.addEventListener('click', () => {
             displayValue.textContent = ""
-            valueArr = [];
+        });
+    } else if (button.textContent === "+" || button.textContent === "-" || button.textContent === "*" || button.textContent === "/" || button.textContent === "="){
+        button.addEventListener('click', () => {
+            if (num1 === ""){
+                num1 = Number(num);
+            } else {
+                num2 = Number(num);
+            }
+            num = "";
+            if (operator === ""){
+                operator = button.textContent;
+            } else if (operator !== "" && num1 !== "" && num2 !== ""){
+                num1 = operate(operator, num1, num2);
+                operator = button.textContent;
+                num2 = "";
+            }
+            displayValue.textContent += button.textContent;
         });
     } else {
         button.addEventListener('click', () => {
             displayValue.textContent += button.textContent;
-            valueArr.push(button.textContent)
+            num += button.textContent;
         });
     }
 });
@@ -38,32 +49,11 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
-    return num1 / num2;
-}
-
-function groupNum(arr){
-    let numArray = []
-    let num = "";
-    console.log(arr);
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] === "+" || arr[i] === "-" || arr[i] === "*" || arr[i] === "/" || arr[i] === "="){
-            num = Number(num);
-            numArray.push(num);
-            numArray.push(arr[i]);
-            num = "";
-        } else {
-            num += arr[i];
-        }
-    }
-    return numArray;
-}
-
-function calculate(numArray){
-    
+    return Math.ceil(num1 / num2);
 }
 
 function operate(operator, num1, num2){
-    let res;
+    let res = "";
     switch (operator){
         case "+":
             res = add(num1, num2);
@@ -75,8 +65,15 @@ function operate(operator, num1, num2){
             res = multiply(num1, num2);
             break;
         case "/":
-            res = divide(num1, num2);
+            if (num2 === 0){
+                displayValue.textContent = "Can't divide by 0";
+                return res;
+            } else {
+                res = divide(num1, num2);
+            }
             break;
     }
+    res = Number(res);
     displayValue.textContent = res;
+    return res;
 }
